f969ee72d4ae28b5828023a257f58ec492c3da64
EC2 Default User <ec2-user@ip-172-31-33-119.us-east-2.compute.internal>
2019-02-20 04:46:41 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,8 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.ratings.map { |rating| {name: rating, value: params[:ratings] ? params[:ratings].include?(rating) : true} }
     @movies = params[:ratings]&.any? ? Movie.find_all_by_ratings(params[:ratings].keys) : Movie.all
-    if params[:sort_by]
-      @movies = Movie.order(params[:sort_by])
+    if params[:sort]
+      @movies = @movies.order(params[:sort])
     end
   end
 
